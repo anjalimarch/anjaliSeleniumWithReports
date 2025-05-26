a5b415756c462ac0844b53276213f2fc08f3bba4
anjalimarch <anjalimarch24@gmail.com>
2025-05-26 18:26:39 +0530

    comiiting project

---
+++ CucumberPOMSeries/test-output-thread/report.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "9483610c-b560-469b-b0fa-75908359785c",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1748263269877,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1748263273609,
+    "className": "passed"
+  },
+  {
+    "id": "d1900064-8414-49cb-b024-a52f35512cd1",
+    "feature": "Login page feature",
+    "scenario": "Login Page title",
+    "start": 1748263263823,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1748263267000,
+    "className": "passed"
+  },
+  {
+    "id": "9dd73e5c-2fdc-4b93-bdde-78c455400e01",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password Link",
+    "start": 1748263267009,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1748263269870,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
